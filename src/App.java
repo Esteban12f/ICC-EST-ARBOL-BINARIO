a34ff454f4af9365bf30794a5f27054e154468ac
Esteban12f <joehv33@gmail.com>
2025-01-08 12:30:08 -0500

    Sin terminar

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import main.materia.controllers.ArbolBinario;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        runArbolBinario();
+    }
+
+    public static void runArbolBinario(){
+        ArbolBinario aB = new ArbolBinario();
+        int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+
+        for (int i = 0; i < valores.length; i++){
+            aB.insert(valores[i]);
+        }
+        aB.printTree();
+    }
+}
