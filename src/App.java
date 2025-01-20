889e2e813d2da7763869a12434da3c2991ad06cd
Esteban12f <joehv33@gmail.com>
2025-01-20 13:05:46 -0500

    no terminado

--- src/App.java
+++ src/App.java
@@ -21,8 +21,8 @@ public class App {
         // runEjercicio(2);
         // runEjercicio(3);
         // runEjercicio(4);
-        runGraph();
-
+        // runGraph();
+        runGraph2();
     }
 
     public static void runArbolBinario(){
@@ -98,18 +98,80 @@ public class App {
 
     public static void runGraph(){
         Graph grafo = new Graph();
-        NodeGraph nodo1 = grafo.addNodeG(5);
-        NodeGraph nodo2 = grafo.addNodeG(7);
-        NodeGraph nodo3 = grafo.addNodeG(9);
-        NodeGraph nodo4 = grafo.addNodeG(11);
-        NodeGraph nodo5 = grafo.addNodeG(3);
-        grafo.addEdge(nodo1, nodo5);
-        grafo.addEdge(nodo1, nodo2);
-        grafo.addEdge(nodo1, nodo3);
-        grafo.addEdge(nodo2, nodo3);
-        grafo.addEdge(nodo5, nodo3);
-        grafo.addEdge(nodo5, nodo4);
+        NodeGraph g2nodo0 = grafo.addNodeG(0);
+        NodeGraph g2nodo1 = grafo.addNodeG(1);
+        NodeGraph g2nodo2 = grafo.addNodeG(2);
+        NodeGraph g2nodo3 = grafo.addNodeG(3);
+        NodeGraph g2nodo4 = grafo.addNodeG(4);
+        NodeGraph g2nodo5 = grafo.addNodeG(5);
+        grafo.addEdge(g2nodo0, g2nodo3);
+        grafo.addEdge(g2nodo0, g2nodo5);
+        grafo.addEdge(g2nodo3, g2nodo2);
+        grafo.addEdge(g2nodo3, g2nodo4);
+        grafo.addEdge(g2nodo4, g2nodo1);
+        grafo.addEdge(g2nodo2, g2nodo1);
+        grafo.addEdge(g2nodo1, g2nodo0);
+
+        //grafo.printGraph();
+
+        Graph grafo2 = new Graph();
+        NodeGraph nodo0 = grafo2.addNodeG(0);
+        NodeGraph nodo1 = grafo2.addNodeG(1);
+        NodeGraph nodo2 = grafo2.addNodeG(2);
+        NodeGraph nodo3 = grafo2.addNodeG(3);
+        NodeGraph nodo4 = grafo2.addNodeG(4);
+        NodeGraph nodo5 = grafo2.addNodeG(5);
+        grafo2.addEdgeUni(nodo0, nodo3);
+        grafo2.addEdgeUni(nodo0, nodo5);
+        grafo2.addEdgeUni(nodo3, nodo2);
+        grafo2.addEdgeUni(nodo3, nodo4);
+        grafo2.addEdgeUni(nodo4, nodo1);
+        grafo2.addEdgeUni(nodo2, nodo1);
+        grafo2.addEdgeUni(nodo1, nodo0);
 
         grafo.printGraph();
+        grafo2.printGraph();
+
+        System.out.println("No dirigido: ");
+        grafo.getDFS(g2nodo0);
+        grafo.getBFS(g2nodo0);
+        System.out.println("");
+
+        System.out.println("Dirigido: ");
+        grafo2.getDFS(nodo0);
+        grafo2.getBFS(nodo0);
+
+
+    }
+
+    public static void runGraph2(){
+        Graph grafo = new Graph();
+        NodeGraph nodo0 = grafo.addNodeG(0);
+        NodeGraph nodo1 = grafo.addNodeG(1);
+        NodeGraph nodo2 = grafo.addNodeG(2);
+        NodeGraph nodo3 = grafo.addNodeG(3);
+        NodeGraph nodo4 = grafo.addNodeG(4);
+        NodeGraph nodo5 = grafo.addNodeG(5);
+        NodeGraph nodo7 = grafo.addNodeG(7);
+        NodeGraph nodo8 = grafo.addNodeG(8);
+        NodeGraph nodo9 = grafo.addNodeG(9);
+
+
+        grafo.addEdge(nodo0, nodo1);
+        grafo.addEdge(nodo0, nodo3);
+        grafo.addEdge(nodo0, nodo5);
+        grafo.addEdge(nodo3, nodo2);
+        grafo.addEdge(nodo3, nodo4);
+        grafo.addEdge(nodo3, nodo7);
+        grafo.addEdge(nodo3, nodo9);
+        grafo.addEdge(nodo2, nodo1);
+        grafo.addEdge(nodo4, nodo1);
+        grafo.addEdge(nodo7, nodo8);
+        grafo.addEdge(nodo1, nodo0);
+        grafo.addEdge(nodo8, nodo1);
+
+        grafo.printGraph();
+
+        grafo.getDFS(nodo0, nodo7);
     }
 }
--- src/main/materia/controllers/Graph.java
+++ src/main/materia/controllers/Graph.java
@@ -1,7 +1,12 @@
 package main.materia.controllers;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 import main.materia.models.NodeGraph;
 
 public class Graph {
@@ -22,6 +27,10 @@ public class Graph {
         dest.addNeighbor(src);
     }
 
+    public void addEdgeUni(NodeGraph src, NodeGraph des){
+        src.addNeighbor(des);
+    }
+
     public void printGraph(){
         for(NodeGraph nodeGraph : nodes){
             System.out.print("vertice: " + nodeGraph.getValue() + ": ");
@@ -32,13 +41,70 @@ public class Graph {
         }
     }
 
-    public void getDFS(NodeGraph startNodeG){
+    public void getDFS(NodeGraph start){
+        Set<NodeGraph> visitados = new HashSet<>();
+        System.out.println("DFS desde el nodo: " + start.getValue());
+        getDFSUtil(start, visitados);
+        System.out.println();
     }
 
-    private void getDFSUtil(NodeGraph node , boolean[] visited){
+    private void getDFSUtil(NodeGraph node , Set<NodeGraph> visitados){
+        if (visitados.contains(node)){
+            return;
+        }
+
+        System.out.print(node.getValue() + " ");
+        visitados.add(node);
+
+        for (NodeGraph neighbor : node.getNeighbors()){
+            getDFSUtil(neighbor, visitados);
+        }
     }
 
-    public void getBFS(NodeGraph startNodeG){
+
+    public void getDFS(NodeGraph start, NodeGraph finish){
+        Set<NodeGraph> visitados = new HashSet<>();
+        System.out.println("DFS desde el nodo: " + start.getValue());
+        getDFSUtil(start, finish, visitados);
+        System.out.println();
+    }
+
+    private void getDFSUtil(NodeGraph node, NodeGraph destino, Set<NodeGraph> visitados){
+        if (visitados.contains(node)){
+            return;
+        }
+
+        System.out.print(node.getValue() + " ");
+        visitados.add(node);
+
+        if (node.equals(destino)){
+            return;
+        }
+
+        for (NodeGraph neighbor : node.getNeighbors()){
+            getDFSUtil(neighbor, visitados);
+        }
+    }
+
+    public void getBFS(NodeGraph start){
+        Set<NodeGraph> visitados = new HashSet<>();
+        Queue<NodeGraph> cola = new LinkedList<>();
+
+        System.out.println("BFS desde el nodo: " + start.getValue());
+        cola.add(start);
+        visitados.add(start);
+
+        while (!cola.isEmpty()) {
+            NodeGraph actual = cola.poll();
+            System.out.print(actual.getValue() + " ");
+
+            for (NodeGraph neighbor : actual.getNeighbors()){
+                if (!visitados.contains(neighbor)){
+                    visitados.add(neighbor);
+                    cola.add(neighbor);
+                }
+            }
+        }
     }
 
     public int[][] getAdjacencyMatrix(){
